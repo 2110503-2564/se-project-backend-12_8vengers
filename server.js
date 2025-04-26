d79767ed030a01c26993f77dbab8255e26c77825
pewpowpow <6733055921@student.chula.ac.th>
2025-04-26 14:39:15 +0700

    add userbalance

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.use(cors());
   app.use('/api/v1/ratings', ratings);
   app.use('/api/v1/payment', protect, payment);
 
-  const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 5003;
 
   const server = app.listen(PORT,console.log('Server running in', process.env.NODE_ENV, 'on '+ process.env.HOST + ":" + PORT));
 
